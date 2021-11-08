ab37249e392ca32d053218d91316f4241fd7d741
Dan Esparza <esparza.dan@gmail.com>
2021-11-08 10:31:37 -0500

    Add code to get and set log state

---
+++ data/state.go
@@ -0,0 +1,116 @@
+package data
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/tidwall/buntdb"
+)
+
+type LogState struct {
+	Unit       string    `json:"unit"`
+	LastCursor string    `json:"last_cursor"`
+	LastSynced time.Time `json:"last_synced"`
+}
+
+// UpdateLogState updates the log state for a given unit
+func (store Manager) UpdateLogState(unit, lastCursor string) (LogState, error) {
+	//	Our return item
+	retval := LogState{
+		Unit:       unit,
+		LastCursor: lastCursor,
+		LastSynced: time.Now(),
+	}
+
+	//	Serialize to JSON format
+	encoded, err := json.Marshal(retval)
+	if err != nil {
+		return retval, fmt.Errorf("problem serializing the data: %s", err)
+	}
+
+	//	Save it to the database:
+	err = store.systemdb.Update(func(tx *buntdb.Tx) error {
+		_, _, err := tx.Set(GetKey("State", unit), string(encoded), nil)
+		return err
+	})
+
+	//	If there was an error saving the data, report it:
+	if err != nil {
+		return retval, fmt.Errorf("problem saving the data: %s", err)
+	}
+
+	//	Return our data:
+	return retval, nil
+}
+
+// GetLogStateForUnit gets log state for a given unit
+func (store Manager) GetLogStateForUnit(unit string) (LogState, error) {
+	//	Our return item
+	retval := LogState{}
+
+	err := store.systemdb.View(func(tx *buntdb.Tx) error {
+		item, err := tx.Get(GetKey("State", unit))
+		if err != nil {
+			return err
+		}
+
+		if len(item) > 0 {
+			//	Unmarshal data into our item
+			val := []byte(item)
+			if err := json.Unmarshal(val, &retval); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	//	If there was an error, report it:
+	if err != nil {
+		return retval, fmt.Errorf("problem getting the log state: %s", err)
+	}
+
+	//	Return our data:
+	return retval, nil
+}
+
+// GetLogStateForAllUnits gets log state for all units
+func (store Manager) GetLogStateForAllUnits() ([]LogState, error) {
+	//	Our return item
+	retval := []LogState{}
+
+	//	Set our prefix
+	prefix := GetKey("State")
+
+	//	Iterate over our values:
+	err := store.systemdb.View(func(tx *buntdb.Tx) error {
+		tx.Descend(prefix, func(key, val string) bool {
+
+			if len(val) > 0 {
+				//	Create our item:
+				item := LogState{}
+
+				//	Unmarshal data into our item
+				bval := []byte(val)
+				if err := json.Unmarshal(bval, &item); err != nil {
+					return false
+				}
+
+				//	Add to the array of returned users:
+				retval = append(retval, item)
+			}
+
+			return true
+		})
+		return nil
+	})
+
+	//	If there was an error, report it:
+	if err != nil {
+		return retval, fmt.Errorf("problem getting log state for all units: %s", err)
+	}
+
+	//	Return our data:
+	return retval, nil
+}
--- go.mod
+++ go.mod
@@ -21,6 +21,14 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/tidwall/btree v0.6.1 // indirect
+	github.com/tidwall/buntdb v1.2.7 // indirect
+	github.com/tidwall/gjson v1.10.2 // indirect
+	github.com/tidwall/grect v0.1.3 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/rtred v0.1.2 // indirect
+	github.com/tidwall/tinyqueue v0.1.1 // indirect
 	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
 	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/ini.v1 v1.63.2 // indirect
